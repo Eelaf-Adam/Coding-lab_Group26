ba07aeef9cfbdb1fe0fb35bfe58dd207fd785425
L-nsamba <l.nsamba@alustudent.com>
2025-06-04 09:01:59 -0700

    Archive and analyze script updates

--- Hospital_mgt_system/archive_logs.sh
+++ Hospital_mgt_system/archive_logs.sh
@@ -3,7 +3,7 @@
 #Defining the variables
 options=("heart_rate" "temperature" "water_usage")
 log_files=("active_logs/heart_rate_log.log" "active_logs/temperature_log.log" "active_logs/water_usage_log.log")
-archive_logs=("heart_data_archive" "temperature_data_archive" "Water_data_archive")
+archive_logs=("heart_data_archive" "temperature_data_archive" "water_data_archive")
 time_stamp=$(date '+%Y-%m-%d %H:%M:%S')
 
 #Creation of numbered menu
@@ -14,11 +14,12 @@ echo "3) water usage"
 read -p "Select log to archive: " log_selected
 
 #Creating respective directories
-echo "Creating directory..."
-mkdir -p heart_data_archive
-mkdir -p temperature_data_archive
-mkdir -p water_usage_data_archive
-
+mkdir -p archived_logs 
+mkdir -p archived_logs
+mkdir -p archived_logs/heart_data_archive
+mkdir -p archived_logs/temperature_data_archive
+mkdir -p archived_logs/water_data_archive
+touch archived_logs/monitoring_files
 #Transfering log file to archive logs
 case $log_selected in
 	1)
@@ -26,20 +27,27 @@ case $log_selected in
      		#Creation of a variable to include timestamp in the new file name
      		archived_logs_filename="heart_rate_${time_stamp}.log"
      		#Moving heart rate log file to archive logs directory and storing it in a new file with time stamp inlcuded in file name
-     		mv "${log_files[0]}" "${archive_logs[0]}/${archived_logs_filename}"
-     		echo "'${log_files[0]}' transfered to '${archive_logs[0]}'"
-     		#Creation of file for continued heart rate monitoring
-     		touch heart_rate_monitor
+     		mv "${log_files[0]}" "archived_logs/${archive_logs[0]}/${archived_logs_filename}"
+     		echo "'${log_files[0]}'successfully transfered to '${archive_logs[0]}'"
      		;;
 	2)
 		echo "Archiving temperature_log.logs..."
 		#Creating of a variable to inclide timestamp in the new file name
-		archived_logs_filename="temperature_${timestamp}.log"
+		archived_logs_filename="temperature_${time_stamp}.log"
 		#Moving temperature log file to archive logs directory and storing it in a new file with time stamp included in file name
-		mv "${log_files[1]}" "${archive_logs[1]}/${archived_logs_filename}"
-		echo "'${log_files[1]} transfered to '${archive_logs[1]}'"
-		#Creation of file for continued heart rate monitoring
-		touch temperature_monitor
+		mv "${log_files[1]}" "archived_logs/${archive_logs[1]}/${archived_logs_filename}"
+		echo "'${log_files[1]} successfully  transfered to '${archive_logs[1]}'"
 		;;
 	3)
-		e
+		echo "Archiving water_usage_log.log..."
+		#Creation of a vraiable to include timestamp in the new file name 
+		archived_logs_filename="water_usage_${time_stamp}.log"
+		#Moving water usage log to archive logs directory and storing it in a new file with time stamp included in file name
+		mv "${log_files[2]}" "archived_logs/${archive_logs[2]}/${archived_logs_filename}"
+		echo "'${log_files[2]}'successfully  transfered to '${archive_logs[2]}'"
+		;;
+       *)
+	       #Error handling 
+	       echo "Invalid selection, please try again"
+	       ;;
+esac
