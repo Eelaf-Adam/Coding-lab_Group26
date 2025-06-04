a90c7402814fba9b62392efabbe1521a6600ff19
Eelaf-Adam <e.adam@alustudent.com>
2025-06-04 04:12:10 -0700

    Update one analysis script

--- Hospital_mgt_system/analyze_logs.sh
+++ Hospital_mgt_system/analyze_logs.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
+
 #Interactive Archival Script
+
+#Creation of reports directory and fil=e
+mkdir -p reports
+touch reports/analysis_report.txt
+
+#Assigning  variables
+choices=("Heart_rate" "temperature" "water_usage")
+time_stamp=$(date '+%Y-%m-%d %H:%M:%S')
+log_file=("heart_rate.log" "temperature.log" "water_usage.log")
+time_
+
+#creating numbered menu
+echo "select log file to analyze: "
+echo "1) Heart rate (heart_rate.log)"
+echo "2) Temperature (temperature.log)"
+echo "3) Water Usage (water_usage.log)"
+read -p "Enter choice (1-3): " user_input
+
+case $user_input in 
+	1) log_files="heart_rate.log"
+	;;
+        2) log_files="temperature_rate.log"
+	;;
+        3) log_files="water_usage.log"
+	;;
+        *) echo "Invalid selection. "
+	exit 
+	;;
+esac
+
